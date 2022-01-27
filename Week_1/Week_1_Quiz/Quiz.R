efd6d7557962ba3fd18a0248232033e366dc9856
Vinicius Araujo Kluska <vinicius@kluska.com.br>
2022-01-27 12:08:42 -0300

    New file - Quiz

---
+++ Week_1/Week_1_Quiz/Quiz.R
@@ -0,0 +1,15 @@
+# Week 1 Quiz
+# Problem 1
+
+    # install.packages("dslabs")
+    library(dslabs)
+    library(dplyr)
+    data("heights")
+    h<-heights$height
+    hist(h, breaks=100)
+
+    # Answer: Normal distrubution
+
+
+
+    
