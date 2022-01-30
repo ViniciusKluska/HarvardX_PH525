0d93c914e35ed83f6dda1f26c6be0ec0b6170719
Vinicius Araujo Kluska <vinicius@kluska.com.br>
2022-01-30 13:02:19 -0300

    New file - probability-Distributios-Exercises.R

--- Week_2/Probability-Distributions-Exercises.R
+++ Week_2/Probability-Distributions-Exercises.R
@@ -1,3 +1,12 @@
 # Pre exercise
 
     install.packages("gapminder")
+    install.packages("ggplot2")
+
+    library(gapminder)
+    library(ggplot2)
+
+    # Load the data
+
+    data(gapminder)
+    head(gapminder)
